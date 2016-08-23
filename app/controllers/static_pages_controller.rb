cf082f47349645ee48b715b501eee5fd878f8bdc
ajessee <andre.isaac.jessee@gmail.com>
2016-08-22 20:12:51 -0400

    setup basic static pages for Head Up site, home, about, contact

--- app/controllers/static_pages_controller.rb
+++ app/controllers/static_pages_controller.rb
@@ -4,4 +4,7 @@ class StaticPagesController < ApplicationController
 
   def contact
   end
+
+  def about
+  end
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,14 +1,33 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
-  test "should get home" do
-    get static_pages_home_url
-    assert_response :success
+
+  def setup
+    @main_title = "Head Up"
   end
 
-  test "should get contact" do
+  test "should get root" do
+    get root_url
+    assert_response :success
+    assert_select "title", "Home | #{@main_title}"
+  end
+
+  test "should get home page" do
+    get static_pages_home_url
+    assert_response :success
+    assert_select "title", "Home | #{@main_title}"
+  end
+
+  test "should get contact page" do
     get static_pages_contact_url
     assert_response :success
+    assert_select "title", "Contact | #{@main_title}"
   end
+
+  test "should get about page" do
+    get static_pages_about_url
+    assert_response :success
+    assert_select "title", "About | #{@main_title}"
+end
 
 end
