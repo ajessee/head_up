8ec55f4d0c552b7589cdea2e970106a82d8f8a4e
ajessee <andre.isaac.jessee@gmail.com>
2016-09-05 22:58:21 -0400

    finish chapter 5 and added user model.

--- config/routes.rb
+++ config/routes.rb
@@ -1,12 +1,9 @@
 Rails.application.routes.draw do
 
+  get 'users/new'
+
   root 'static_pages#home'
-
-  get 'static_pages/home'
-
-  get 'static_pages/contact'
-
-  get 'static_pages/about'
+  get  '/signup',  to: 'users#new'
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -7,27 +7,9 @@ class StaticPagesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "should get root" do
-    get root_url
+    get root_path
     assert_response :success
     assert_select "title", "#{@main_title}"
   end
-
-  test "should get home page" do
-    get static_pages_home_url
-    assert_response :success
-    assert_select "title", "#{@main_title}"
-  end
-
-  test "should get contact page" do
-    get static_pages_contact_url
-    assert_response :success
-    assert_select "title", "Contact | #{@main_title}"
-  end
-
-  test "should get about page" do
-    get static_pages_about_url
-    assert_response :success
-    assert_select "title", "About | #{@main_title}"
-end
 
 end
