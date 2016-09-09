b463bfd40c1e2319ea024e39f3b7161195c38f1d
ajessee <andre.isaac.jessee@gmail.com>
2016-09-09 14:35:30 -0400

    start user sign up

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,9 @@
 Rails.application.routes.draw do
 
-  get 'users/new'
-
   root 'static_pages#home'
+
   get  '/signup',  to: 'users#new'
+  resources :users
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
