1df98dc88f22b1ef1c9574b38d61f9a561790630
ajessee <andre.isaac.jessee@gmail.com>
2016-10-09 23:59:32 -0400

    fix fonts 8

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,8 +54,6 @@ Rails.application.configure do
   # Suppress logger output for asset requests.
   config.assets.quiet = true
 
-  config.assets.paths << Rails.root.join("assets", "fonts")
-
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
