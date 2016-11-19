c2cf57dff28e04d6c92915bf8aca66e30d77a5dd
ajessee <andre.isaac.jessee@gmail.com>
2016-11-19 17:21:23 -0500

    add advanced login with permanent cookies

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160915005608) do
+ActiveRecord::Schema.define(version: 20161119000320) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -30,6 +30,7 @@ ActiveRecord::Schema.define(version: 20160915005608) do
     t.datetime "created_at",      null: false
     t.datetime "updated_at",      null: false
     t.string   "password_digest"
+    t.string   "remember_digest"
     t.index ["email"], name: "index_users_on_email", unique: true, using: :btree
   end
 
