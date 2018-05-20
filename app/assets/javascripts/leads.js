4244958ea2a02f16280eea1c73a67332ccc25353
ajessee <andre.isaac.jessee@gmail.com>
2018-05-19 23:15:20 -0400

    update one more icon, and prevent default on leads form

--- app/assets/javascripts/leads.js
+++ app/assets/javascripts/leads.js
@@ -1,2 +1,7 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+
+$('.btn-outline-dark').click(function(event){
+  event.preventDefault();
+  alert('Sorry, this website has been deprecated and is only for demo purposes now')
+})
